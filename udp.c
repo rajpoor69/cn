3b09dbd20942626ed86a94677e8e2ef12700a4ad
rajpoor69 <pappadam946@gmail.com>
2024-07-07 23:57:03 +0530

    Update udp.c

--- udp.c
+++ udp.c
@@ -1,84 +1,99 @@
 SERVER
 
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 7000
+#define BUFFER_SIZE 2000
+
 int main() {
-  int server;
-  char servMsg[2000], cliMsg[2000];
-  struct sockaddr_in servAddr, client_addr;
-  int client_struct_length = sizeof(client_addr);
-  server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (server < 0) {
-    printf("Error while creating socket\n");
-    exit(1);
-  }
-  printf("Socket created successfully\n");
-  servAddr.sin_family = AF_INET;
-  servAddr.sin_port = htons(2002);
-  servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  if (bind(server, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
-    printf("Couldn't bind to the port\n");
-    exit(1);
-  }
-  printf("Binding done\n");
-  printf("Listening...\n");
-  if (recvfrom(server, cliMsg, sizeof(cliMsg), 0,
-               (struct sockaddr *)&client_addr, &client_struct_length) < 0) {
-    printf("Couldn't receive\n");
-    exit(1);
-  }
-  printf("Msg from client: %s\n", cliMsg);
-  strcpy(servMsg, cliMsg);
-  if (sendto(server, servMsg, strlen(servMsg), 0,
-             (struct sockaddr *)&client_addr, client_struct_length) < 0) {
-    printf("Can't send\n");
-    exit(1);
-  }
-  close(server);
-  return 0;
+    int server;
+    struct sockaddr_in servAddr, clientAddr;
+    char buffer[BUFFER_SIZE];
+    socklen_t clientLen = sizeof(clientAddr);
+    ssize_t recv_len, send_len;
+
+    if ((server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(PORT);
+
+    if (bind(server, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+        perror("Bind failed");
+        close(server);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    if ((recv_len = recvfrom(server, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &clientAddr, &clientLen)) < 0) {
+        perror("Receive failed");
+        close(server);
+        exit(EXIT_FAILURE);
+    }
+    buffer[recv_len] = '\0';
+
+    printf("Message from client: %s\n", buffer);
+
+    if ((send_len = sendto(server, buffer, recv_len, 0, (struct sockaddr *) &clientAddr, clientLen)) != recv_len) {
+        perror("Send failed");
+        close(server);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Message sent back to client.\n");
+
+    close(server);
+    return 0;
 }
 
 CLIENT
 
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
-void main() {
-  int client;
-  struct sockaddr_in servAddr;
-  char servMsg[2000], cliMsg[2000];
-  int server_struct_length = sizeof(servAddr);
-  client = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+#include <arpa/inet.h>
 
-  if (client < 0) {
-    printf("Error while creating socket\n");
-    exit(1);
-  }
-  printf("Socket created successfully\n");
-  servAddr.sin_family = AF_INET;
-  servAddr.sin_port = htons(2002);
-  servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  printf("Enter message to server: ");
-  gets(cliMsg);
-  if (sendto(client, cliMsg, strlen(cliMsg), 0, (struct sockaddr *)&servAddr,
-             sizeof(servAddr)) < 0) {
-    printf("Unable to send message\n");
-    exit(1);
-  }
-  //(socklen_t *)&server_struct_length
-  if (recvfrom(client, servMsg, sizeof(servMsg), 0,
-               (struct sockaddr *)&servAddr, &server_struct_length) < 0) {
-    printf("Error while receiving server's msg\n");
-    exit(1);
-  }
-  close(client);
+#define PORT 7000
+#define BUFFER_SIZE 2000
+#define SERVER_IP "127.0.0.1"
+
+int main() {
+    int client;
+    struct sockaddr_in servAddr;
+    char buffer[BUFFER_SIZE];
+    socklen_t addr_len = sizeof(servAddr);
+
+    if ((client = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &servAddr.sin_addr);
+
+    printf("Enter message to server: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+
+    if (sendto(client, buffer, strlen(buffer), 0, (struct sockaddr*)&servAddr, addr_len) < 0) {
+        perror("Message sending failed");
+        exit(EXIT_FAILURE);
+    }
+
+    recvfrom(client, buffer, BUFFER_SIZE, 0, NULL, NULL);
+    printf("Server response: %s\n", buffer);
+
+    close(client);
+    return 0;
 }
-
